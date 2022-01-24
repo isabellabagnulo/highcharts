123e64bd50b8694b0e76c22d43ae55c5e30d0473
isabellabagnulo <77104271+isabellabagnulo@users.noreply.github.com>
2022-01-24 10:07:48 +0100

    burger

---
+++ burger.js
@@ -0,0 +1,26 @@
+function burgermenu() {
+  const menu = document.querySelector(".menu")
+  const burger = document.querySelector(".burger-menu")
+  const open = document.querySelector(".open")
+  const close = document.querySelector(".close")
+
+  burger.addEventListener("click", toggleMenu)
+  function toggleMenu() {
+    if (menu.classList.contains("expanded")) {
+      menu.classList.remove("expanded")
+      close.style.display = "none"
+      open.style.display = "block"
+    } else {
+      menu.classList.add("expanded")
+      close.style.display = "block"
+      open.style.display = "none"
+    }
+  }
+
+  const menuLinks = document.querySelectorAll(".menu-link")
+  menuLinks.forEach(function (menuLink) {
+    menuLink.addEventListener("click", toggleMenu)
+  })
+}
+
+document.addEventListener("DOMContentLoaded", burgermenu)
